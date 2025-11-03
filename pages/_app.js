2efc4c29552471d5fd86b7e6c69e4381ae9add84
alice <ryutoiwa2929@gmail.com>
2025-11-03 16:14:15 +0900

    GitHub Pagesで背景画像が表示されるように修正

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // basePathを考慮した背景画像のパスを設定
+    const basePath = router.basePath || ''
+    const backgroundImageUrl = `${basePath}/background.jpeg`
+    
+    document.body.style.backgroundImage = `url('${backgroundImageUrl}')`
+    
+    return () => {
+      // クリーンアップ（必要に応じて）
+    }
+  }, [router.basePath])
+
   return <Component {...pageProps} />
 }
